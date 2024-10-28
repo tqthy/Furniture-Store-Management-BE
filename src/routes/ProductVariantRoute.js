5533cf9d16d2cb91c74571f78c1850c50b5c0ded
Tam-Vu <vuthanhtam12062003@gmail.com>
2024-10-28 09:06:57 +0700

    get productvariant

--- src/routes/ProductVariantRoute.js
+++ src/routes/ProductVariantRoute.js
@@ -6,8 +6,8 @@ const ProductVariantRoute = (app) => {
     router.post('/create-variant/:productId', ProductVariantController.createProductVariant);
     router.put('/update-variant/:id', ProductVariantController.updateProductVariant);
     router.delete('/delete-variant/:id', ProductVariantController.deleteProductVariant);
-    router.get('/get-all-variants/:productId', ProductVariantController.getAllProductVariants);
-    router.get('/get-variant/:id', ProductVariantController.getAllProductVariantsByProductId);
+    router.get('/get-all-variants/:productId', ProductVariantController.getAllProductVariantsByProductId);
+    router.get('/get-variant/:id', ProductVariantController.getProductVariantById);
     router.get('/', ProductVariantController.getAllProductVariants)
     return app.use('/variants', router);
 }
