581f8c615ed920bcb49a6a883e755e7d97b2c624
Thy Tran <22521461@gm.uit.edu.vn>
2024-11-17 14:48:23 +0700

    feat: get sale staff report
    Needs Invoice to be done for testing this API

--- src/services/ReportService.js
+++ src/services/ReportService.js
@@ -10,7 +10,8 @@ class ReportService {
               LEFT JOIN InvoiceDetails ON ProductVariant.id = InvoiceDetails.variantId
               LEFT JOIN Invoice ON InvoiceDetails.invoiceId = Invoice.id
               WHERE Invoice.createdAt BETWEEN :fromDate AND :toDate
-              GROUP BY ProductVariant.id`,
+              GROUP BY ProductVariant.id
+              ORDER BY sumCost DESC`,
               {
                 replacements: {
                   fromDate: fromDate,
@@ -33,7 +34,43 @@ class ReportService {
             };
         }
     }
-
+  
+    getSaleStaffReport = async (fromDate, toDate) => {
+        try {
+            const report = await db.sequelize.query(
+              `SELECT Staff.id staffId, 
+                Staff.fullname staffName, 
+                COUNT(Invoice.id) countInvoice, 
+                SUM(Invoice.total) sumTotal,
+                SUM(InvoiceDetails.quantity) sumQuantity
+                FROM Staff
+                LEFT JOIN Invoice ON Staff.id = Invoice.staffId
+                LEFT JOIN InvoiceDetails ON Invoice.id = InvoiceDetails.invoiceId
+                WHERE Invoice.createdAt BETWEEN :fromDate AND :toDate
+                AND Staff.role = 'Sale
+                GROUP BY Staff.id'`,
+              {
+                replacements: {
+                  fromDate: fromDate,
+                  toDate: toDate
+                },
+                type: db.sequelize.QueryTypes.SELECT
+              }
+            )
+            
+            return {
+                EM: 'Get report successfully',
+                EC: 0,
+                DT: report
+            };
+        } catch (error) {
+            return {
+                EM: error.message,
+                EC: 1,
+                DT: ''
+            };
+        }
+    }
 
 }
 
